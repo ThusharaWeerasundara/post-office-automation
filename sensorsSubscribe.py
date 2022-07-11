7689c67689e7fe5a345c857861454728948ab114
thushara weerasundara <e16388@eng.pdn.ac.lk>
2022-07-12 00:54:22 +0530

    python subscriber added

--- sensorsSubscribe.py
+++ sensorsSubscribe.py
@@ -6,7 +6,8 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ygTest")
+    client.subscribe("ygTest/weight")
+    client.subscribe("ygTest/cost")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
